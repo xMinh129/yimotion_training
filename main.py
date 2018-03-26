0d7d36cb4df8b42ca7098198e81e7002516ec887
xuanminh12995 <minh.vu@u.nus.edu>
2018-03-26 10:23:01 +0300

    Revert "First commit"
    This reverts commit 16b4e104f9307bfd8ebc279c03bc56954c82b323.

--- main.py
+++
@@ -1,8 +0,0 @@
-import glob
-from object_detection.run_detection_model import run_detection_model
-
-PATH_TO_TEST_IMAGES_DIR = '/Users/minhvu/Downloads/yimotion_training/eval_images/*.jpg'
-TEST_IMAGE_PATHS = glob.glob(PATH_TO_TEST_IMAGES_DIR)
-
-emotion_labels = run_detection_model(TEST_IMAGE_PATHS)
-print(emotion_labels)
